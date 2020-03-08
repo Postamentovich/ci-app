98cc6736349f2f1a9298b61063213f9e678b388b
postamentovich <postamentovich@gmail.com>
2020-03-09 00:39:17 +0300

    refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ function browser() {
 
 function scss() {
   return gulp
-    .src("./scss/*.scss")
+    .src("./scss/**/*.scss")
     .pipe(sass())
     .pipe(concatCss("dist/style.css"))
     .pipe(gulp.dest("./"))
@@ -26,6 +26,6 @@ function scss() {
 
 exports.default = function() {
   browser();
-  gulp.watch("./scss/*.scss", scss);
+  gulp.watch("./scss/**/*.scss", scss);
   gulp.watch("dist/*.html").on('change', browserSync.reload);
 };

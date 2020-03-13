9af8deb230b44a15343d4b81d4a9c63fc37afdef
Postamentovich <postamentovich@gmail.com>
2020-03-13 21:41:06 +0300

    update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 require('dotenv').config()
-const schoolAPI = require('./school-api')
+const storageAPI = require('./storage-api')
 
 const app = express()
 
@@ -11,7 +11,7 @@ app.use(express.json())
  */
 app.get('/api/settings', async (req, res) => {
   try {
-    const config = await schoolAPI.getConfig()
+    const config = await storageAPI.getConfig()
 
     res.json(config)
   } catch (error) {
@@ -29,7 +29,7 @@ app.get('/api/settings', async (req, res) => {
 app.post('/api/settings', async (req, res) => {
   try {
     console.log('body', req.body)
-    const result = schoolAPI.setConfig(req.body)
+    const result = storageAPI.setConfig(req.body)
     res.status(200)
 
     res.json(result)
@@ -45,7 +45,15 @@ app.post('/api/settings', async (req, res) => {
 /**
  * Получение списка сборок
  */
-app.get('/api/builds', (req, res) => {})
+app.get('/api/builds', async (req, res) => {
+  try {
+    const result = await storageAPI.getBuildList()
+    res.send(result)
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(500)
+  }
+})
 
 /**
  * Добавление сборки в очередь

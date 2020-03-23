cd894ba2f316f73a5c4dbe2a8dff7f819c276b58
postamentovich <postamentovich@gmail.com>
2020-03-23 23:00:58 +0300

    add redux

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,17 @@
 import React, { FC } from 'react';
 import { compose, composeU } from '@bem-react/core';
 
-import { Button as ButtonPresenter } from './Components/Button/Button';
-import { withButtonTypeLink } from './Components/Button/_type/Button_type_link';
-import { withButtonThemeAction } from './Components/Button/_theme/Button_theme_action';
+import { Button as ButtonPresenter } from './Component/Button/Button';
+import { withButtonTypeLink } from './Component/Button/_type/Button_type_link';
+import { withButtonThemeAction } from './Component/Button/_theme/Button_theme_action';
 
 const Button = compose(composeU(withButtonThemeAction), withButtonTypeLink)(ButtonPresenter);
 
 const App: FC = () => (
   <div className="App">
-    <Button>I'm basic</Button>
+    <Button>m basic</Button>
     <Button type="link" href="#stub">cxzc</Button>
-    <Button theme="action">I'm theme action</Button>
+    <Button theme="action">m theme action</Button>
     <Button theme="action" type="link">cxzc</Button>
   </div>
 );

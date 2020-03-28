17ed026f7d43b8c134c62924a76ce6a24d4e6633
postamentovich <postamentovich@gmail.com>
2020-03-29 01:39:22 +0300

    add Title

---
+++ client/src/components/Title/_type/Title_type_h1.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { withBemMod } from '@bem-react/core';
+import { ITitleProps, cnTitle } from '../index';
+
+export interface ITitleTypeH1Props {
+  /**
+   * Тип заголовка
+   */
+  type?: 'h1';
+}
+
+/**
+ * Модификатор, отвечающий за тип заголовка
+ */
+export const withTitleTypeH1 = withBemMod<ITitleTypeH1Props, ITitleProps>(cnTitle(), { type: 'h1' }, Title => props => (
+  <Title {...props} as="h1" />
+));
--- client/src/containers/Header/Header.tsx
+++ client/src/containers/Header/Header.tsx
@@ -1,10 +1,17 @@
 import React, { FC } from 'react';
-
+import { Title as TitlePresenter } from 'components/Title/Title';
 import { IHeaderProps, cnHeader } from './index';
+import { withTitleTypeH1 } from 'components/Title/_type/Title_type_h1';
+import { withTitleViewDefault } from 'components/Title/_view/Title_view_default';
+import { compose } from '@bem-react/core';
+
+const Title = compose(withTitleViewDefault, withTitleTypeH1)(TitlePresenter);
 
 export const Header: FC<IHeaderProps> = ({ children, className, as: Component = 'header', title, ...props }) => (
   <Component {...props} className={cnHeader({}, [className])}>
-    <h1 className={cnHeader('Title')}>{title}</h1>
+    <Title type="h1" className={cnHeader('Title')} view="default">
+      {title}
+    </Title>
     <div className={cnHeader('Buttons')}>{children}</div>
   </Component>
 );

3b9707f88e2495d885803df52444d7d9a233a8c9
postamentovich <postamentovich@gmail.com>
2020-04-04 02:24:37 +0300

    убрал скролл для модального окна

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { IModalProps, cnModal } from './index';
 import './Modal.scss';
@@ -7,6 +7,13 @@ import './Modal.scss';
  * Компонент для создания модальных окон
  */
 export const Modal: FC<IModalProps> = ({ children, className, as: Component = 'div', ...props }) => {
+  useEffect(() => {
+    document.body.classList.add('body-no-scroll');
+
+    return () => {
+      document.body.classList.remove('body-no-scroll');
+    };
+  }, []);
   return ReactDOM.createPortal(
     <div className={cnModal()}>
       <Component className={cnModal('Content', [className])} {...props}>
